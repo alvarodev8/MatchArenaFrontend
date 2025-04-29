a5f2ad25080070136b943db515fc6f4b2adca9f1
alvarodev8 <alvarodev8@gmail.com>
2025-04-30 01:46:47 +0200

    configure main routes with layouts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { JugadorLayoutComponent } from './layouts/jugador-layout/jugador-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: 'jugador',
+        component: JugadorLayoutComponent,
+        children: [
+            { path: '', loadChildren: () => import('./features/jugador/jugador.routes').then(r => r.jugadorRoutes) }
+        ]
+    },
+    { path: '', redirectTo: 'jugador', pathMatch: 'full' }
+];
