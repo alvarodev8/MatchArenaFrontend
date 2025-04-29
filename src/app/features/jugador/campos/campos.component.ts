3dac425f0ab6505f968a5842f9f43b92404474e3
alvarodev8 <alvarodev8@gmail.com>
2025-04-30 01:52:22 +0200

    add football pitches component for player feature

---
+++ src/app/features/jugador/campos/campos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-campos',
+  imports: [],
+  templateUrl: './campos.component.html',
+  styleUrl: './campos.component.scss'
+})
+export class CamposComponent {
+
+}
