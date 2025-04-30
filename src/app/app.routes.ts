ba8d99da1d4d4464a328831b3df7b3760c277301
alvarodev8 <alvarodev8@gmail.com>
2025-04-30 11:10:20 +0200

    add public layout and auth routes with guard for player layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,25 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from './core/guards/auth.guard';
+import { PublicLayoutComponent } from './layouts/public-layout/public-layout.component';
 import { JugadorLayoutComponent } from './layouts/jugador-layout/jugador-layout.component';
 
 export const routes: Routes = [
-
+    {
+        path: '',
+        component: PublicLayoutComponent,
+        children: [
+            { path: '', loadChildren: () => import('./features/auth/auth.routes').then(r => r.authRoutes) },
+            { path: 'login', loadChildren: () => import('./features/auth/auth.routes').then(r => r.authRoutes) },
+            { path: 'register', loadChildren: () => import('./features/auth/auth.routes').then(r => r.authRoutes) }
+        ]
+    },
     {
         path: 'jugador',
         component: JugadorLayoutComponent,
+        canActivate: [AuthGuard],
         children: [
             { path: '', loadChildren: () => import('./features/jugador/jugador.routes').then(r => r.jugadorRoutes) }
         ]
     },
-    { path: '', redirectTo: 'jugador', pathMatch: 'full' }
+    { path: '**', redirectTo: '' }
 ];
