d2d9697d2720e3b8dc8acfe38a5640f80b762ba3
alvarodev8 <alvarodev8@gmail.com>
2025-05-16 00:49:54 +0200

    implement player reservation component

--- src/app/features/jugador/jugador.routes.ts
+++ src/app/features/jugador/jugador.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 import { PerfilComponent } from './perfil/perfil.component';
-import { PartidosComponent } from './partidos/partidos.component';
+import { ReservasComponent } from './reservas/reservas.component';
 import { CamposComponent } from './campos/campos.component';
 
 export const jugadorRoutes: Routes = [
     { path: 'perfil', component: PerfilComponent },
-    { path: 'partidos', component: PartidosComponent },
+    { path: 'reservas', component: ReservasComponent },
     { path: 'campos', component: CamposComponent },
     { path: '', redirectTo: 'perfil', pathMatch: 'full' }
 ];
