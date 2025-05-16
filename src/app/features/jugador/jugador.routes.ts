8bf0f15299c9be420fbc83c37e3ba2f5e2c27f7e
alvarodev8 <alvarodev8@gmail.com>
2025-05-16 22:11:41 +0200

    add route for detail functionality

--- src/app/features/jugador/jugador.routes.ts
+++ src/app/features/jugador/jugador.routes.ts
@@ -3,11 +3,13 @@ import { PerfilComponent } from './perfil/perfil.component';
 import { ReservasComponent } from './reservas/reservas.component';
 import { CamposComponent } from './campos/campos.component';
 import { ReservaFormComponent } from './campos/reservar/reservar.component';
+import { CampoDetallesComponent } from './campos/detalle/detalle.component';
 
 export const jugadorRoutes: Routes = [
     { path: 'perfil', component: PerfilComponent },
     { path: 'reservas', component: ReservasComponent },
     { path: 'campos', component: CamposComponent },
+    { path: 'campos/:id/detalles', component: CampoDetallesComponent },
     { path: 'reserva', component: ReservaFormComponent },
     { path: 'reserva/:pitchId', component: ReservaFormComponent },
     { path: '', redirectTo: 'perfil', pathMatch: 'full' }
