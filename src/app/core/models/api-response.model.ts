ab49d9c032f15b9c3913b2bcd187834d2acdca25
alvarodev8 <alvarodev8@gmail.com>
2025-06-06 01:29:32 +0200

    add api-response model to handle standardized API responses

---
+++ src/app/core/models/api-response.model.ts
@@ -0,0 +1,43 @@
+import { User } from "./user.model";
+import { Pitch } from "./pitch.model";
+import { Reservation } from "./reservation.model";
+
+export interface UsersResponse {
+    message: string;
+    users: User[];
+}
+
+export interface UserResponse {
+    message: string;
+    user: User;
+}
+
+export interface PitchesResponse {
+    message: string;
+    pitches: Pitch[];
+}
+
+export interface ReservationsResponse {
+    message: string;
+    reservations: Reservation[];
+}
+
+export interface ReservationResponse {
+    message: string;
+    reservation: Reservation;
+}
+
+export interface AvailableDatesResponse {
+    message: string;
+    dates: { date: string; available: boolean }[];
+}
+
+export interface AvailableTimesResponse {
+    message: string;
+    availableTimes: string[];
+}
+
+export interface AvailabilityResponse {
+    message?: string;
+    available: boolean;
+}
