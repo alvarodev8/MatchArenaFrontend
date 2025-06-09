993d2d5787f53434e3e4ea2a5dfb0cd22578a098
alvarodev8 <alvarodev8@gmail.com>
2025-06-09 12:20:12 +0200

    integrate LoadingComponent in ReservasComponent

--- src/app/features/jugador/reservas/reservas.component.ts
+++ src/app/features/jugador/reservas/reservas.component.ts
@@ -2,17 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { Reservation } from '../../../core/models/reservation.model';
 import { ReservasService } from './reservas.service';
 import { CommonModule } from '@angular/common';
+import { LoadingComponent } from '../../../shared/components/loading/loading.component';
 
 @Component({
   selector: 'app-jugador-reservas',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LoadingComponent],
   templateUrl: './reservas.component.html',
   styleUrl: './reservas.component.scss'
 })
 export class ReservasComponent implements OnInit {
   reservations: Reservation[] = [];
   error: string | null = null;
+  isLoading: boolean = true;
 
   constructor(private reservasService: ReservasService) { }
 
@@ -22,7 +24,8 @@ export class ReservasComponent implements OnInit {
       error: (err) => {
         this.error = 'Error al obtener las reservas: ' + (err.error?.message || err.message);
         console.error('Error al obtener reservas:', err);
-      }
+      },
+      complete: () => this.isLoading = false
     });
   }
 }
