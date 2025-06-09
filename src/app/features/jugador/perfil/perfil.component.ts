89e63e69a6e16e9a65e169718a58c43804f3a10d
alvarodev8 <alvarodev8@gmail.com>
2025-06-09 12:19:59 +0200

    integrate LoadingComponent in PerfilComponent

--- src/app/features/jugador/perfil/perfil.component.ts
+++ src/app/features/jugador/perfil/perfil.component.ts
@@ -2,17 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { PerfilService } from './perfil.service';
 import { User } from '../../../core/models/user.model';
 import { CommonModule } from '@angular/common';
+import { LoadingComponent } from '../../../shared/components/loading/loading.component';
 
 @Component({
   selector: 'app-jugador-perfil',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LoadingComponent],
   templateUrl: './perfil.component.html',
   styleUrl: './perfil.component.scss'
 })
 export class PerfilComponent implements OnInit {
   user: User | null = null;
   error: string | null = null;
+  isLoading: boolean = true;
 
   constructor(private perfilService: PerfilService) { }
 
@@ -22,7 +24,8 @@ export class PerfilComponent implements OnInit {
       error: (err) => {
         this.error = 'Error al obtener el perfil: ' + (err.error?.message || err.message);
         console.error('Error al obtener el perfil:', err);
-      }
+      },
+      complete: () => this.isLoading = false
     });
   }
 }
