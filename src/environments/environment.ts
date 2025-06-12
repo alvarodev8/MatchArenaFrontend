fc437ea1fbff878ab81ccb01f159f6e2820c3c58
alvarodev8 <alvarodev8@gmail.com>
2025-06-12 18:09:44 +0200

    update environment.ts with stripePublicKey

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8000/api'
+    apiUrl: 'http://localhost:8000/api',
+    stripePublicKey: 'pk_test_51RYEUFRC8m9ymNwVY3ExAjTC3B8eO3vGskCeKxVGbEjHNDTAJxWntvIM5ZefEftv9B71bsMDQYg6HbgOML8dg6ZD00aI2hH8nh',
 };
