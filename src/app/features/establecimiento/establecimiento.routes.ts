26b4bb38e35ada4e7be36e9620bfbd1b9cd6cc3d
alvarodev8 <alvarodev8@gmail.com>
2025-06-13 16:15:35 +0200

    add establecimiento routes for pitch management

--- src/app/features/establecimiento/establecimiento.routes.ts
+++ src/app/features/establecimiento/establecimiento.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { CamposComponent } from './campos/campos.component';
+import { AddPitchComponent } from './campos/add-pitch/add-pitch.component';
+import { EditPitchComponent } from './campos/edit-pitch/edit-pitch.component';
 
 export const establecimientoRoutes: Routes = [
     { path: 'campos', component: CamposComponent },
+    { path: 'campos/add', component: AddPitchComponent },
+    { path: 'campos/edit/:id', component: EditPitchComponent },
     { path: '', redirectTo: 'campos', pathMatch: 'full' }
 ];
