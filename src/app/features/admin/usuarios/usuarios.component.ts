d042d828d6e6213e6a25ea91abc59a33dd809a28
alvarodev8 <alvarodev8@gmail.com>
2025-06-15 22:28:33 +0200

    implement component to list and manage users

--- src/app/features/admin/usuarios/usuarios.component.ts
+++ src/app/features/admin/usuarios/usuarios.component.ts
@@ -3,6 +3,7 @@ import { UsuariosService } from './usuarios.service';
 import { CommonModule } from '@angular/common';
 import { LoadingComponent } from '../../../shared/components/loading/loading.component';
 import { User } from '../../../core/models/user.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-usuarios',
@@ -16,13 +17,38 @@ export class UsuariosComponent implements OnInit {
   error: string | null = null;
   isLoading: boolean = true;
 
-  constructor(private usuariosService: UsuariosService) { }
+  constructor(private usuariosService: UsuariosService, private router: Router) { }
 
   ngOnInit(): void {
+    this.loadUsuarios();
+  }
+
+  loadUsuarios(): void {
     this.usuariosService.getUsuarios().subscribe({
       next: (response) => this.users = response.users,
       error: (err) => this.error = 'Error al cargar los usuarios: ' + (err.error?.message || err.message),
       complete: () => this.isLoading = false,
     });
+  }
+
+  viewReservations(userId: number): void {
+    this.router.navigate([`/admin/usuarios/${userId}/reservations`]);
+  }
+
+  crearUsuario() {
+    this.router.navigate(['/admin/usuarios/create'])
+  }
+
+  editUser(userId: number): void {
+    this.router.navigate([`/admin/usuarios/${userId}/edit`]);
+  }
+
+  deleteUser(userId: number): void {
+    if (confirm('¿Estás seguro de eliminar este usuario?')) {
+      this.usuariosService.deleteUsuario(userId).subscribe({
+        next: () => this.loadUsuarios(),
+        error: (err) => this.error = 'Error al eliminar el usuario: ' + (err.error?.message || err.message),
+      });
+    }
   }
 }
