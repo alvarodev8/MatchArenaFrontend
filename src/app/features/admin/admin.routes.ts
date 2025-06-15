b4d10bb91eb8f4d9e07a1647efb1c62beaa76a6f
alvarodev8 <alvarodev8@gmail.com>
2025-06-15 22:28:04 +0200

    update user model and define routes for administrator management

--- src/app/features/admin/admin.routes.ts
+++ src/app/features/admin/admin.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { UsuariosComponent } from './usuarios/usuarios.component';
+import { EditUsuarioComponent } from './usuarios/edit-usuario/edit-usuario.component';
+import { CreateUsuarioComponent } from './usuarios/create-usuario/create-usuario.component';
+import { UserReservationsComponent } from './usuarios/user-reservations/user-reservations.component';
 
 export const adminRoutes: Routes = [
     { path: 'usuarios', component: UsuariosComponent },
+    { path: 'usuarios/:id/edit', component: EditUsuarioComponent },
+    { path: 'usuarios/create', component: CreateUsuarioComponent },
+    { path: 'usuarios/:id/reservations', component: UserReservationsComponent },
     { path: '', redirectTo: 'usuarios', pathMatch: 'full' }
 ];
